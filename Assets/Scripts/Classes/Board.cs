ce559f7d0d55269a84c028601e6f3700295617b8
Ahtat204 <lahcen28ahtat@gmail.com>
2025-07-22 15:37:00 +0200

    used Grid and Tilemap for pieces coordinates

--- Assets/Scripts/Classes/Board.cs
+++ Assets/Scripts/Classes/Board.cs
@@ -1,12 +1,54 @@
 ﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+using Assets.Scripts.Structs;
 
 namespace Assets.Scripts.Classes
-
 {
-    public class Board
+    public class Board : MonoBehaviour
     {
-        public static float size;
+        public  Tilemap tilemap; // Assign in Inspector
+        private Coordinates Position;
+        public Vector3 mousePosWorld;
+        public Vector3Int mouseCell;
+        public Vector3 mouseCellInterpolated;
 
-        
+
+        private void Awake()
+        {
+            
+
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                /*
+                Debug.Log("Mouse clicked");
+                // Convert screen to world
+                mousePosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePosWorld.z = 0f; // Important in 2D
+
+                // Get cell position
+                mouseCell = tilemap.WorldToCell(mousePosWorld);
+                mouseCellInterpolated = tilemap.LocalToCellInterpolated(tilemap.transform.InverseTransformPoint(mousePosWorld));
+
+                   Debug.Log("Cell: " + mouseCell);
+                  Debug.Log("Interpolated: " + mouseCellInterpolated);
+                  */
+                Position = GetCoordinates();
+                Debug.Log("Mouse clicked at: " + Position.ToString());
+            }
+        }
+
+        public virtual Coordinates GetCoordinates()
+        {
+            System.Diagnostics.Debug.Assert(Camera.main != null, "Camera.main != null");
+            var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPos.z = 0f;
+
+            var cell = tilemap.WorldToCell(mouseWorldPos);
+            return new Coordinates(cell.x, cell.y);
+        }
     }
 }
--- Assets/Scripts/Structs/Coordinates.cs
+++ Assets/Scripts/Structs/Coordinates.cs
@@ -1,12 +1,31 @@
-﻿using System;
-
-namespace Assets.Scripts.Structs
+﻿namespace Assets.Scripts.Structs
 {
     public struct Coordinates
     {
-        public uint X;
-        public uint Y;
+        private int x, y;
 
-  
+        public int X
+        {
+            get => x;
+            set => x = value;
+        }
+
+        public int Y
+        {
+            get => y;
+            set => y = value;
+        }
+
+     public Coordinates(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            if (x == 0 || y == 0) throw new System.Exception("Invalid coordinates");
+          
+        }
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
     }
 }
