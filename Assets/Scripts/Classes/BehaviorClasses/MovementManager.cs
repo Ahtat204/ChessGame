3dc4224afb4cff891c3e58917711a8eafa3d203b
Ahtat204 <lahcen28ahtat@gmail.com>
2025-10-18 12:48:01 +0200

    fixed direct Piece Capturing functionality

--- Assets/Scripts/Classes/BehaviorClasses/MovementManager.cs
+++ Assets/Scripts/Classes/BehaviorClasses/MovementManager.cs
@@ -2,64 +2,78 @@
 using System.Collections.Generic;
 using System.Linq;
 using Assets.Scripts.Classes.GameClasses;
+using Assets.Scripts.Classes.Pieces;
 using Assets.Scripts.Enums;
-using Assets.Scripts.Classes;
-using Unity.VisualScripting;
 using UnityEngine;
-
 
 namespace Assets.Scripts.Classes.BehaviorClasses
 {
-    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Piece))]
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(SpriteRenderer))]
+    [RequireComponent(typeof(SelectableDecorator))]
     internal sealed class MovementManager : MonoBehaviour
     {
         private SelectableDecorator _selectableDecorator;
         private Piece _piece;
         private Rigidbody2D _rigidbody;
         private Vector3 _target;
-        private List<MovementManager> _movementManagers= new (32);
-        public Vector3Int CurrPos{get; private set;}
-        public bool HasMoved { get; private set; }
+        private bool _canCapture;
+        private Vector3Int CurrPos { get; set; }
+        public bool HasMoved { get; set; }
 
         private void Awake()
         {
-            _movementManagers.Add(this);
             _selectableDecorator = GetComponent<SelectableDecorator>();
             _rigidbody = GetComponent<Rigidbody2D>();
             _piece = GetComponent<Piece>();
             _target = transform.position;
             HasMoved = false;
-            
         }
 
         private void Start()
         {
-            CurrPos=Board.BoardInstance.tilemap.WorldToCell(transform.position);
+            CurrPos = Board.BoardInstance.tilemap.WorldToCell(transform.position);
         }
 
-        public void MovePiece()
+        private void Update()
+        {
+            HandleInput();
+            MovePiece();
+        }
+        private void HandleInput()
         {
             if (Input.GetMouseButtonDown(0) && _selectableDecorator.Status == SelectionStatus.Selected)
             {
                 _target = Board.BoardInstance.MainCamera.ScreenToWorldPoint(Input.mousePosition);
                 _target.z = 0;
             }
-            var target = Board.BoardInstance.tilemap.WorldToCell(_target);
-            var tar = Board.BoardInstance.tilemap.GetCellCenterWorld(target);
-            _rigidbody.MovePosition(_piece.PossibleMoves.Contains((Vector2Int)target) ? tar : transform.position);
-            HasMoved=CurrPos != target;
-            
-                 CurrPos = target;
-             if (HasMoved)
-             {
-                 _selectableDecorator.Status = SelectionStatus.UnSelected;
-             }
-             HasMoved = false;
         }
 
-        private void Update()
+        private void MovePiece()
         {
-            MovePiece();
+            var targetCell = Board.BoardInstance.tilemap.WorldToCell(_target);
+            var worldCellCenter = Board.BoardInstance.tilemap.GetCellCenterWorld(targetCell);
+            if (!_piece.PossibleMoves.Contains((Vector2Int)targetCell)) return;
+            _rigidbody.MovePosition(worldCellCenter);
+            if ( targetCell != CurrPos)
+            {
+                CurrPos = targetCell;
+                HasMoved = true;
+                _selectableDecorator.Status = SelectionStatus.UnSelected;  
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            _canCapture = !other.gameObject.CompareTag(gameObject.tag) && !other.gameObject.CompareTag("King");
+            var mom=other.GetComponent<MovementManager>(); //this was the critical line
+            if (!_canCapture) return;
+            if (HasMoved && mom is not null && !mom.HasMoved)
+            {
+                Destroy(other.gameObject);
+            }
+            HasMoved = false;
         }
     }
 }
