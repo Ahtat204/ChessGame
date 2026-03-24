694f39271f043e7c5e1b2665dc5fcc4566e97dad
Lahcen <lahcen28ahtat@gmail.com>
2026-03-24 22:11:30 +0100

    update ,WIP

--- Assets/Scripts/Classes/PieceComponent/PieceSelectionComponent.cs
+++ Assets/Scripts/Classes/PieceComponent/PieceSelectionComponent.cs
@@ -11,8 +11,6 @@ namespace Assets.Scripts.Classes.PieceComponent
     //: this class is working correctly
     [RequireComponent(typeof(BoxCollider2D))]
     [RequireComponent(typeof(Piece))]
-    
-    
     public class PieceSelectionComponent : MonoBehaviour, ISelectable
     {
         public SelectionStatus Status { get; set; }
@@ -20,31 +18,33 @@ namespace Assets.Scripts.Classes.PieceComponent
         public static event onPieceSelected OnPieceSelected;
         public Vector2 Target => target;
         public int Count { get; set; }
-       
         private static readonly List<PieceSelectionComponent> MovableObjects = new();
         public Vector2 target;
+
         private void Awake()
         {
             MovableObjects.Add(this);
             Status = SelectionStatus.UnSelected;
-            
         }
+
         private void Start()
         {
             target = transform.position;
         }
-        public void OnSelect() {
+
+        public void OnSelect()
+        {
             Status = SelectionStatus.Selected;
             OnPieceSelected?.Invoke();
         }
-        
-        public void OnDeselect(){
+
+        public void OnDeselect()
+        {
             Status = SelectionStatus.UnSelected;
         }
-        
+
         private void OnMouseDown()
         {
-            
             if (Status == SelectionStatus.Selected) OnDeselect();
             else OnSelect();
             foreach (var obj in MovableObjects.Where(obj => obj != this))
@@ -52,13 +52,13 @@ namespace Assets.Scripts.Classes.PieceComponent
                 obj.Status = SelectionStatus.UnSelected;
             }
         }
+
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0) && Status == SelectionStatus.Selected )
+            if (Input.GetMouseButtonDown(0) && Status == SelectionStatus.Selected)
             {
                 target = Board.BoardInstance.MainCamera.ScreenToWorldPoint(Input.mousePosition);
             }
         }
     }
-
 }
