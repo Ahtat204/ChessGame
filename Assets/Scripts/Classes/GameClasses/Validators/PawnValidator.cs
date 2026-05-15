de877c15248fd1381f80939453b63fa5c8f4c9ca
Lahcen <lahcen28ahtat@gmail.com>
2026-05-15 17:59:05 +0200

    added an first prototype for a static method responsible for checking if a pawn can capture

--- Assets/Scripts/Classes/GameClasses/Validators/PawnValidator.cs
+++ Assets/Scripts/Classes/GameClasses/Validators/PawnValidator.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using Assets.Scripts.Classes.PieceComponent;
+using UnityEngine;
 
 namespace Assets.Scripts.Classes.GameClasses.Validators
 {
-    public class PawnValidator : MonoBehaviour
+    public static class PawnValidator
     {
-        
+        public static bool ValidateCapturing(this Dictionary<Vector2Int, PieceMovementComponent> pieces, Vector2Int start,
+            Vector2Int end)
+        {
+            var piece = pieces.GetValueOrDefault(end);
+            
+            return piece is not null;
+        }
     }
 }
